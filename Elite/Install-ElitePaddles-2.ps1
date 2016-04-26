1b61a518a50f6b97d5d68d32e240bc473dcd81e2
mhvuze <vuze.mhf2resg@gmail.com>
2016-04-26 09:39:43 +0200

    VS Workaround

--- Elite/Install-ElitePaddles-2.ps1
+++ Elite/Install-ElitePaddles-2.ps1
@@ -13,13 +13,11 @@ if(-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrat
 
 Write-Warning @"
 Running this script will perform the following actions on your PC:
-1. Modify the dependencies of the project files to point to the directory of your XboxDevices app.
-2. Compile Elite.sln. It is expected that the projects were not modified from how they were packeged with the solution.
-3. Compile the ElitePaddlesServiceHost which must be run simultaneously with ElitePaddles and acts as an HTTP listener for SendInput commands (http://localhost:8642/EliteService)
-4. Register the above url for the active user. Existing registration will be removed as it is assumed to be stale state.
-5. Generate certificate to sign the appx package. The user will be prompted for passwords to create the certs, and then again to use them (4 prompts).
-6. Add the certificate to the root store and sign the appx package.
-7. Deploy the appx package.
+1. Compile the ElitePaddlesServiceHost which must be run simultaneously with ElitePaddles and acts as an HTTP listener for SendInput commands (http://localhost:8642/EliteService)
+2. Register the above url for the active user. Existing registration will be removed as it is assumed to be stale state.
+3. Generate certificate to sign the appx package. The user will be prompted for passwords to create the certs, and then again to use them (4 prompts).
+4. Add the certificate to the root store and sign the appx package.
+5. Deploy the appx package.
 "@
 
 Write-Host "Press any key to continue ..."
@@ -75,18 +73,6 @@ if(-not ((Test-Path $makeCertLocation -PathType Leaf) -and (Test-Path $pvk2pfxLo
 {
     throw "Could not find makecert.exe, pvk2pfx.exe, or signtool.exe in C:\Program Files*\Windows Kits\10\bin\x86\. Please make sure you have the Windows 10 SDK installed (https://dev.windows.com/en-us/downloads/windows-10-sdk)."
 }
-
-# Assume the csproj files exists and change their dependencies to point to the Xbox Accessories app directory
-$eliteUiCsprojLocation = $slnDir + "\EliteUi\EliteUi.csproj"
-if ($xboxDevicesLocation)
-{
-    $xml = [xml](Get-Content -Path $eliteUiCsprojLocation)
-    $xml.Project.ItemGroup.Reference | ? { $_.HintPath -and $_.HintPath.Contains("XboxDevices") } | % { $file = $_.HintPath | Split-Path -Leaf; $_.HintPath = $xboxDevicesLocation + "\" + $file }
-    $xml.Save($eliteUiCsprojLocation)
-}
-
-# Build the solution
-& $msbuildLocation $SlnPath /m:4 /t:Rebuild "/p:Configuration=Release;OutDir=.\Out\;Platform=${procArch};AppxPackageSigningEnabled=false"
 
 # Set up app package paths and make sure the installation script was generated
 $eliteLocation = $slnDir + "\EliteUi\Out\EliteUi\AppPackages\EliteUi_1.0.0.0_${procArch}_Test\"
