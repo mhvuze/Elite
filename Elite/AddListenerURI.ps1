05d9a46e7a6f47ef5b7dd09e7dadd4f605ef34e3
Pierre Krafft <kpierre@outlook.com>
2017-01-19 22:07:58 +0100

    Preserve original user in AddListenerURI
    Elevating the command causes the current user to be overwritten if it doesn't have Administrator access. Preserving the original user enables the EliteServiceHost to run without extra permissions.

--- Elite/AddListenerURI.ps1
+++ Elite/AddListenerURI.ps1
@@ -1,6 +1,10 @@
+param (
+    [string]$user = "$env:userdomain\$env:username",
+ )
 # Acquire admin
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" -ArgumentList -user $user" -Verb RunAs; exit }
 
 # Set listener URI Reservation
-netsh http add urlacl url=http://+:8642/EliteService user=$env:userdomain\$env:username
-Read-Host -Prompt "Added URI Reservation.`nPress any key to exit"
+netsh http delete urlacl url=http://+:8642/EliteService
+netsh http add urlacl url=http://+:8642/EliteService user=$user
+Read-Host -Prompt "Added URI Reservation for $user.`nPress any key to exit"
